c39f9f3b8f10da63737bc2908784d802c8785e88
ramkhanjm-art <ramkhanjm@gmail.com>
2026-03-17 07:40:29 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener("install", function(event) {
-  console.log("Service Worker Installed");
-});
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js');
+}
